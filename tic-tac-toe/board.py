1d10fc5bf6392ae53a2d06e27e05e964f4ac8cd9
JonahVisscher <Jonahvisscher@gmail.com>
2024-12-10 21:25:52 +0100

    creation of board.py and main.py (both unimplemented)

---
+++ tic-tac-toe/board.py
@@ -0,0 +1,14 @@
+class Board:
+
+    board = [
+        ['','',''],
+        ['','',''],
+        ['','','']
+    ]
+
+    def update(self, column, row, player):
+        pass
+
+    def reset(self):
+        pass
+
